c1b6e1e6e018040228d5c5d9fb6f6ed8be3c576e
Michael Lambert <mlamber7@its.jnj.com>
2014-04-09 12:24:37 -0400

    CM-1000 added comment to file

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,8 +10,8 @@ module.exports = function (grunt) {
 	// read the package file to retrieve our build configuration variables
 	var pkg = require('./package.json');
 
-	// load the tasks from seperate files located in tasks/options and tasks/custom-options
-	// setting in tasks/custom-options override those in task/options as they are resolved last
+	// load the tasks from seperate files located in tasks/options and tasks/custom-options;
+	// settings in tasks/custom-options override those in task/options 
 	var config = grunt.util._.extend({},
 		require('load-grunt-config')(grunt, {
 			configPath: path.join(__dirname, 'tasks/options'),
@@ -24,7 +24,8 @@ module.exports = function (grunt) {
 			init: false
 		})
 	);
-	// place package variables into grunt config so that they may be retrieved from tasks (e.g. <%= pkg.properties.hostname %>)
+	// place package variables into grunt config so that they may be retrieved from tasks 
+	// (e.g. <%= pkg.properties.hostname %>)
 	config.pkg = pkg;
 
 	// allow env variables to be retrievable
